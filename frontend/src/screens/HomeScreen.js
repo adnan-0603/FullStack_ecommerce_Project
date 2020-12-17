ed876e1b3fb5b780964f2b6d228e4117959429a2
Respite_0603 <adnan.chowdhury01@northsouth.edu>
2020-12-17 15:03:35 +0600

    loading,danger message added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import Product from '../components/Product';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
+import axios from      'axios';
+import Product from    '../components/Product';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
 
 export default function HomeScreen() {
+  const [products ,  setProducts]  =  useState([]);
+  const [loading  ,   setLoading]  =  useState(false);
+  const [error    ,     setError]  =  useState(false);
+
+  useEffect(() => {
+      const fetchData = async () =>{                           /*send a request */
+       try{
+          setLoading(true);
+          const { data } = await axios.get('/api/products');   /*the array of backend will be transferred to front end */
+          setLoading(false);
+          setProducts(data);                                   /*setting products what we are getting from backend */
+    } catch(err) {
+          setError(err.message);
+          setLoading(false);
+    }
+  };
+    fetchData(); //calling fetch data 
+  },[]);
+
     return (
-        <div>
-       <div className = "row center">
-         {
-           data.products.map(product =>(
-           <Product key = {product._id} product = {product}></Product>
-           
-           ))}
-          
-             </div>                     
-           </div>     
-    )
-}
+       <div>
+         {loading ? (
+           <LoadingBox></LoadingBox>
+         ) : error ? (
+           <MessageBox varient = "danger"> {error}</MessageBox>
+         ) : (
+            <div className = "row center">
+              {products.map((product) => (
+                <Product key = {product.id} product ={product}></Product>
+              ))}
+              </div>
+         )}
+
+            </div>
+    );
+  }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -54,7 +54,7 @@ export default function ProductScreen(props){
                                 {product.countInStock > 0? (
                                      <span className = "success">In stock</span>
                                 ):(
-                                    <span className ="error">Unavailable</span>
+                                    <span className ="danger">Unavailable</span>
 
                                 )}
                             </div>
